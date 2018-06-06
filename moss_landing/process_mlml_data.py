53059eccfb8d92daac8211c0e0cec7beb49ee0c7
Tom Connolly <tconnolly@mlml.calstate.edu>
2018-06-06 15:29:55 -0700

    reorganized data file structures

--- moss_landing/process_mlml_data.py
+++ moss_landing/process_mlml_data.py
@@ -11,10 +11,12 @@ import mlml_data_path
 # get paths of data files
 # paths to data should be modified in mlml_data_path.py
 sw_dir = mlml_data_path.moss_landing()
-nc_dir = mlml_data_path.netcdf()
 
 # path to final netcdf files
-nc_dir = os.path.join(nc_dir,'moss_landing')
+sw_csv_dir = os.path.join(sw_dir,'csv/')
+
+# path to final netcdf files
+sw_nc_dir = os.path.join(sw_dir,'netcdf/')
 nc_prefix = 'moss_landing_'
 
 run_download = False
@@ -23,13 +25,13 @@ run_makencfile = True
 if run_download:
     
     # download csv files and convert to NetCDF format
-    mlml.download_station_data(sw_dir,'seawater',overwrite=True)
+    mlml.download_station_data(sw_csv_dir,'seawater',overwrite=True)
     
 if run_makencfile:
     
-    if not os.path.exists(nc_dir):
-        os.mkdir(nc_dir)
+    if not os.path.exists(sw_nc_dir):
+        os.mkdir(sw_nc_dir)
 
-    mlml.make_netcdf(sw_dir,nc_dir,nc_prefix,'seawater',download=False)
+    mlml.make_netcdf(sw_csv_dir,sw_nc_dir,nc_prefix,'seawater',download=False)
 
         
